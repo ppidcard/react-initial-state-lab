af5e3dbc27b80be3f8847e2927b441853b4ce236
ppidcard <tvxqlikei@live.cn>
2021-07-31 13:28:14 +1000

    Done

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
+import React, { Component } from "react";
+
 // your Bomb code here!
+class Bomb extends Component{
+    constructor(props){
+        super();
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    render(){
+        return(
+            
+            this.state.secondsLeft == 0 ? <div>Boom!</div> : <div>{this.state.secondsLeft} seconds left before I go boom!</div>
+            
+        )
+    }
+}
+
+export default Bomb
